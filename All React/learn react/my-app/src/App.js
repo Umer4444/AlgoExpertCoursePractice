5b94ce063034bae6dc11bfdb4ff5e7aabb7f8da2
unknown <mumermalik1245@gmail.com>
2023-04-25 23:23:50 +0500

    last comment

---
+++ All React/learn react/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello</p>
+      <SayHello/>
+    </div>
+  );
+}
+function SayHello() {
+  return <p>Umer</p>
+}
+
+export default App;
